a8da6a72b05380fe79a41c21bf621d0cd97a7c7a
adhikarisushant <sushant.adhikari.619@gmail.com>
2022-04-27 12:51:05 +0545

    first commit

---
+++ server/server.js
@@ -0,0 +1,36 @@
+import path from 'path'
+import express from "express";
+import template from '../../template';
+import { MongoClient } from 'mongodb'
+
+//comment out before building for production
+import devBundle from './devBundle'
+ 
+const app = express()
+//comment out before building for production
+devBundle.compile(app)
+
+const CURRENT_WORKING_DIR = process.cwd()
+app.use('/dist', express.static(path.join(CURRENT_WORKING_DIR, 'dist')))
+
+app.get('/', (req, res) => {
+ res.status(200).send(template())
+})
+
+let port = process.env.PORT || 3000
+
+app.listen(port, function onStart(err) {
+    if (err) {
+    console.log(err)
+    }
+    console.info('Server started on port %s.', port)
+})
+
+
+// Database Connection URL
+const url = process.env.MONGODB_URI || 'mongodb://localhost:27017/mernSimpleSetup'
+// Use connect method to connect to the server
+MongoClient.connect(url, { useNewUrlParser: true, useUnifiedTopology: true },(err, db)=>{
+  console.log("Connected successfully to mongodb server")
+  db.close()
+})
